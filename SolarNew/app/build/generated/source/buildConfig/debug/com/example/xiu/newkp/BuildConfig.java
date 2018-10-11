d4dcd220df2e1b80725b158900676ba9a5f4412a
Tong Anh Quan <anhquantong77@gmail.com>
2018-10-11 10:53:53 +0700

    Testing

---
+++ SolarNew/app/build/generated/source/buildConfig/debug/com/example/xiu/newkp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.xiu.newkp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.xiu.newkp";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
